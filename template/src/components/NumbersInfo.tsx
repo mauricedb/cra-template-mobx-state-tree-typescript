46ef180c981f47a65917429f6ab1c2a6b7dd8e00
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-12-13 19:18:20 +0100

    observer() instead of useObserver()

--- template/src/components/NumbersInfo.tsx
+++ template/src/components/NumbersInfo.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { useObserver } from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 
 import { useNumbersStore } from "../models";
 
 const NumbersInfo: React.FunctionComponent = () => {
   const numbersStore = useNumbersStore();
 
-  return useObserver(() => {
-    return (
-      <div>
-        <div>Sum: {numbersStore.sum}</div>
-        <div>Product: {numbersStore.product}</div>
-        <div>Average: {numbersStore.average}</div>
-      </div>
-    );
-  });
+  return (
+    <div>
+      <div>Sum: {numbersStore.sum}</div>
+      <div>Product: {numbersStore.product}</div>
+      <div>Average: {numbersStore.average}</div>
+    </div>
+  );
 };
 
-export default NumbersInfo;
+export default observer(NumbersInfo);
--- template/src/components/NumbersList.tsx
+++ template/src/components/NumbersList.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { useObserver } from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 
 import { useNumbersStore } from "../models";
 
 const NumbersList: React.FunctionComponent = () => {
   const numbersStore = useNumbersStore();
 
-  return useObserver(() => {
-    return (
-      <ul className="Numbers-list">
-        {numbersStore.numbers.map((value, index) => (
-          <li key={index}>{value}</li>
-        ))}
-      </ul>
-    );
-  });
+  return (
+    <ul className="Numbers-list">
+      {numbersStore.numbers.map((value, index) => (
+        <li key={index}>{value}</li>
+      ))}
+    </ul>
+  );
 };
 
-export default NumbersList;
+export default observer(NumbersList);
